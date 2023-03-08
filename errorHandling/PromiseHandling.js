c234471bc9323962ad81b5cbf607f114a287f40f
Maha1208 <Mahalakshmi.raja@aspiresys.com>
2023-03-08 14:21:23 +0530

    type Script

--- errorHandling/PromiseHandling.js
+++ errorHandling/PromiseHandling.js
@@ -1,9 +1,10 @@
 function doSomethingAsync() {
     return new Promise((resolve, reject) => {
       // Simulate an async operation that fails
+
       setTimeout(() => {
         reject(new Error('async error'))
-      }, 6000)
+      }, 2000)
     })
   }
   
