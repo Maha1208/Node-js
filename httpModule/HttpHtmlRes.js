2eaeda2559f7aaedce59fa51cb623669c4e6f5c3
Maha1208 <Mahalakshmi.raja@aspiresys.com>
2023-03-09 15:30:22 +0530

    commit

--- httpModule/HttpHtmlRes.js
+++ httpModule/HttpHtmlRes.js
@@ -5,13 +5,10 @@ const fs= require("node:fs");
 const server=http.createServer((req,res)=>{
     const name="Maha"
     res.writeHead(200,{"Content-Type":"text/html"});
-    // const html =fs.readFileSync("./HttpModule.html","utf-8");
-    // res.end(html)
     let html= fs.readFileSync("./HttpModule.html","utf-8");
     html=html.replace("{{name}}",name);
     res.end(html);
-   // // fs.createReadStream(__dirname + "/HttpModule.html").pipe(res)
-    // res.end("<h1>Hello Node JS</h1>");
+  
 });
 
 server.listen(4000,()=>{
